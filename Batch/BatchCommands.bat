f42db58354a254cd41f45a99aad3c6e373322f64
Panjagala_Veera_Pavan <documentprofesionally@gmail.com>
2023-08-05 15:12:44 +0530

    Update BatchCommands.bat

--- Batch/BatchCommands.bat
+++ Batch/BatchCommands.bat
@@ -15,3 +15,25 @@ net use \\<remoteservername>\C$ /user:Domain_Name\Domain_User Domainpwd
 robocopy /sec /s /e /j /Z /NOOFFLOAD /W:10 /MT:16 "%cd%" \\<remoteservername>\C$\targefolder /xd "<providefoldernamewhichareneededtoexcludefromcopying>"
 IF %ERRORLEVEL% EQU 1 exit /B 0
 
+
+REM batch script is used to download artifacts from nexus
+
+@echo off
+
+setlocal
+
+set packageUrl=<artifacturl>
+set destinationPath=<directorypath>
+set token=<usertoken>
+set passcode=<pwdtoken>
+
+curl -o "%destinationPath%" -H "Authorization: Bearer %token% %passcode%" "%packageUrl%"
+
+if %errorlevel% equ 0 (
+    echo Package downloaded successfully.
+) else (
+    echo Failed to download package.
+)
+
+endlocal
+
