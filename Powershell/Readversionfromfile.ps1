66ecbf955e0c8da7064dbd760823a4b9dfac41ff
Panjagala_Veera_Pavan <documentprofesionally@gmail.com>
2023-08-05 15:51:33 +0530

    Create Readversionfromfile.ps1

---
+++ Powershell/Readversionfromfile.ps1
@@ -0,0 +1,24 @@
+﻿$dynamicPart = Get-ChildItem "C:\filepath" | Where-Object { $_.Name -like "filename*.csv" } | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+if ($dynamicPart) {
+$value = Get-Content $dynamicPart.FullName | Select-String -Pattern "version2.0" | ForEach-Object { $_.Matches.Value }
+if ($value) {
+$uniqueValue = $value | Select-Object -Unique
+$outputFile = "C:\Version.txt"
+
+New-Item -Path $outputFile -ItemType File -Force
+Set-Content -Path $outputFile -Value $uniqueValue
+
+Write-Host $uniqueValue
+
+# Move "output.txt" to Jenkins agent
+$jenkinsAgentPath = "\\Jenkinsagentvmname\C$\specificfoldername"
+copy-Item -Path $outputFile -Destination "$jenkinsAgentPath\Version.txt"
+
+# Delete the file from the Windows VM
+Remove-Item -Path $outputFile
+} else {
+Write-Host "Error: Value 'version2.0' not found in the .csv file."
+}
+} else {
+Write-Host "Error: No matching .csv files found."
+}
