fb6ee632ace55395218b8d59b7fa128431075685
Panjagala Pavan <documentprofesionally@gmail.com>
2023-12-23 15:40:14 +0530

    Delete Powershell/copyfile.ps1

--- Powershell/copyfile.ps1
+++
@@ -1,4 +0,0 @@
-$remotePath = "\\<remoteservername>\<sharedfoldername>$\<Targetdirectory>\package.sha256.sha256"
-$localPath = "C:\package\package.sha256.sha256"
-
-Copy-Item -Path $remotePath -Destination $localPath -Force
